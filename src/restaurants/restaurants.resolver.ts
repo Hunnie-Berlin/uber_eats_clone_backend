f5f19114c577d27df850eb6a112e03ba15571ed5
CHEOL HUN LEE <doulos2008@naver.cpm>
2021-01-07 15:53:33 +0100

    setup GraphQL Module

---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,18 @@
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
+
+@Resolver(() => Restaurant)
+export class RestaurantResolver {
+  @Query(() => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    console.log(veganOnly);
+    return [];
+  }
+
+  @Mutation(() => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    console.log(createRestaurantDto);
+    return true;
+  }
+}
